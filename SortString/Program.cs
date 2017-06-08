a77e6770a16eb3e4194ba5936868bf7d5c2e2371
Atsidir <tamas.torjai@gmail.com>
2017-06-08 09:15:08 +0200

    Add project files.

---
+++ SortString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SortString
+{
+    static class Program
+    {
+        public static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.Write(word);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+            Program.PrintWords(words);
+            Array.Sort(words);
+            Program.PrintWords(words);
+
+            Console.WriteLine(str);
+            str = string.Join(" ", words);
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
